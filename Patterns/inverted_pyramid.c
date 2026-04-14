52087445ae6981d663068358bedf56d43f836323
VinuthaST-vincy <vinuthastvincy@gmail.com>
2026-04-14 13:10:06 +0530

    Add inverted pyramid pattern in C

---
+++ Patterns/inverted_pyramid.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n=4;
+    for(int i=n;i>=1;i--){
+        for(int s=1;s<=n-i;s++){
+            printf(" ");
+        }
+        for(int j=1;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
