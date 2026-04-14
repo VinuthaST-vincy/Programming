8bb4338a76044752348e4f29e011461ea8c45ff6
VinuthaST-vincy <vinuthastvincy@gmail.com>
2026-04-14 19:11:00 +0530

    Update pascals_triangle.c

--- Patterns/pascals_triangle.c
+++ Patterns/pascals_triangle.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main(){
+  int n=5;
+  int val;
+  for(int i=0;i<=n-1;i++){
+   val=1;
+    for(int j=0;j<=i;j++){
+      printf("%d ",val);
+      val=val*(i-j)/(j+1);
+    }
+    printf("/n");
+}
